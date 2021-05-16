6efcef4dbbac965b96f8be27791d0a6ad8a91636
waldek wocial <waldiwocial@hotmail.com>
2021-05-16 10:55:15 +0100

    creating layouts

---
+++ 11_freecamp/main.py
@@ -0,0 +1,67 @@
+import os
+
+from kivy.uix.widget import Widget
+os.environ["KIVY_NO_ARGS"] = "1"
+os.environ['KIVY_IMAGE'] = "pil,sdl2" # use pil instead of SDL2 image if you get the libpng16 error
+# you must add to the path the location of your SDL2 binaries
+os.environ['PATH'] += ';' + os.path.expandvars('%AppData%\\Python\\share\\glew\\bin')
+os.environ['PATH'] += ';' + os.path.expandvars('%AppData%\\Python\\share\\sdl2\\bin')
+
+from kivy.app import App
+
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.stacklayout import StackLayout
+
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.lang import Builder
+
+from kivy.metrics import dp
+
+#kv = Builder.load_file("stylesheet.kv")
+
+class StackLayoutExample(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        #self.orientation = 'rl-tb'
+        for x in range(100):
+            #size = dp(100) + x*10
+            size = dp(100)
+            b = Button(text=str(x+1), size_hint=(None,None), size=(size,size))
+            self.add_widget(b)
+
+# class GridLayoutExample(GridLayout):
+#     pass
+
+class AnchorLayoutExample(AnchorLayout):
+    pass
+
+class BoxLayoutExample(BoxLayout):
+    pass
+'''
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.orientation ='vertical'
+        b1 = Button(text = 'A')
+        b2 = Button(text = 'B')
+        b3 = Button(text = 'C')
+        self.add_widget(b1)
+        self.add_widget(b2)
+        self.add_widget(b3)
+'''
+
+class MainWidget(Widget):
+    pass
+
+# class MyApp(App):
+#     def build(self):
+#         return MainWidget()
+
+class myApp(App):
+    pass
+
+if __name__ == '__main__':
+    myApp().run()
