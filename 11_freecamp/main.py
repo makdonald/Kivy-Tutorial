e42260eb4a452f8d205caa059c46c0231999dbb8
waldek wocial <waldiwocial@hotmail.com>
2021-05-17 17:48:27 +0100

    canvas examples

--- 11_freecamp/main.py
+++ 11_freecamp/main.py
@@ -16,7 +16,10 @@ from kivy.uix.stacklayout import StackLayout
 
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
-from kivy.properties import StringProperty, BooleanProperty
+from kivy.properties import StringProperty, BooleanProperty, Clock
+
+from kivy.graphics.vertex_instructions import Line, Rectangle, Ellipse
+from kivy.graphics.context_instructions import Color
 #from kivy.lang import Builder
 
 
@@ -97,6 +100,88 @@ class MainWidget(Widget):
 
 class myApp(App):
     pass
+
+class CanvasExample1(Widget):
+    pass
+
+class CanvasExample4(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        with self.canvas:
+            Line(points=(100, 100, 400, 500), width=2)
+            Color(0,1,0)
+            Line(circle=(400, 200, 80), width=2)
+            Line(rectangle=(500,300,150,100), width=5)
+            # create instance of rectangle for move_rect()
+            self.rect = Rectangle(pos=(500,100), size=(150,100))
+
+    def move_rect(self):
+        print('clicked the button', self.width, self.height)
+        x, y = self.rect.pos
+        w, h = self.rect.size
+        inc = dp(10)
+
+        diff = self.width - (x+w)
+        if diff < inc:
+            inc = diff
+
+        x += inc
+        y += inc
+        self.rect.pos = (x,y)
+
+        # if y < self.height - h and x < self.width - w and y > 0 and x > 0:
+        #     x -= inc
+        #     y -= inc
+            
+        #     self.rect.pos = (x, y)
+        # else:
+        #     print('stop moving because its a boundry of screen')
+
+class CanvasExample5(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.ball_size = dp(50)
+        self.vx = dp(3)
+        self.vy = dp(3)
+        with self.canvas:
+            self.ball = Ellipse(pos=self.center, size=(self.ball_size, self.ball_size))
+        Clock.schedule_interval(self.update, 1/90)
+
+    def on_size(self, *args):
+        print("on size : " + str(self.width) + ", " + str(self.height))
+        #self.ball.pos = (self.width/2, self.height/2)
+        self.ball.pos = (self.center_x - self.ball_size/2, self.center_y - self.ball_size/2)
+
+    def update(self, dt):
+        #print("update")
+        x, y = self.ball.pos
+        x += self.vx
+        y += self.vy
+
+        if y + self.ball_size > self.height:
+            y = self.height - self.ball_size
+            self.vy = - self.vy
+
+        if x + self.ball_size > self.width:
+            x = self.width - self.ball_size
+            self.vx = -self.vx
+
+        if y < 0:
+            y = 0
+            self.vy = -self.vy
+
+        if x < 0:
+            x = 0
+            self.vx = -self.vx
+
+        self.ball.pos = (x, y)
+
+class CanvasExample6(Widget):
+    pass
+
+
+
+
 
 if __name__ == '__main__':
     myApp().run()
